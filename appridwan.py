7d30616c43c64f2143db0b915fa7e045101b347e
ridwankurniawanx <65314308+ridwankurniawanx@users.noreply.github.com>
2024-04-03 19:31:12 +0700

    Update appridwan.py

--- appridwan.py
+++ appridwan.py
@@ -14,12 +14,16 @@ if __name__=="__main__":
   data = [["OHL1BCUMEASUREMENT1/powMMXU1.TotW.mag.f","MX"],["OHL1BCUMEASUREMENT1/powMMXU1.TotVAr.mag.f","MX"],["OHL1BCUCONTROL1/CSWI1.Pos.stVal","ST"]]
 
   if (error == iec61850.IED_ERROR_OK):
-    daread = data[2][0]
-    dafc = eval(str("iec61850.IEC61850_FC_"+str(data[2][1])))
-    readobj=iec61850.IedConnection_readObject(con,daread,dafc)
-    print(str(daread+" "+str(readobj)))
-    val=iec61850.MmsValue_getBitStringAsIntegerBigEndian(readobj[0])
-    print(val)
+    for i in range(len(data)):
+      daread = data[2][0]
+      dafc = eval(str("iec61850.IEC61850_FC_"+str(data[2][1])))
+      readobj=iec61850.IedConnection_readObject(con,daread,dafc)
+      print(str(daread+" "+str(readobj)))
+      if (data[i][1]=="ST"):
+        val=iec61850.MmsValue_getBitStringAsIntegerBigEndian(readobj[0])
+      else:
+        val=iec61850.MmsValue_toFloat(readobj[0])
+      print(val)
     iec61850.IedConnection_close(con)
   iec61850.IedConnection_destroy(con)
     
