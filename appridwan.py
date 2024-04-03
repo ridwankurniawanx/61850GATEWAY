32c9b083a3c50949e66cb375621853feab545c2d
ridwankurniawanx <65314308+ridwankurniawanx@users.noreply.github.com>
2024-04-03 19:40:32 +0700

    Update appridwan.py

--- appridwan.py
+++ appridwan.py
@@ -14,16 +14,18 @@ if __name__=="__main__":
   data = [["OHL1BCUMEASUREMENT1/powMMXU1.TotW.mag.f","MX"],["OHL1BCUMEASUREMENT1/powMMXU1.TotVAr.mag.f","MX"],["OHL1BCUCONTROL1/CSWI1.Pos.stVal","ST"]]
 
   if (error == iec61850.IED_ERROR_OK):
-    for i in range(len(data)):
-      daread = data[i][0]
-      dafc = eval(str("iec61850.IEC61850_FC_"+str(data[i][1])))
-      readobj=iec61850.IedConnection_readObject(con,daread,dafc)
+    for i in range(100):
+      for i in range(len(data)):
+        daread = data[i][0]
+        dafc = eval(str("iec61850.IEC61850_FC_"+str(data[i][1])))
+        readobj=iec61850.IedConnection_readObject(con,daread,dafc)
       
-      if (data[i][1]=="ST"):
-        val=iec61850.MmsValue_getBitStringAsIntegerBigEndian(readobj[0])
-      else:
-        val=iec61850.MmsValue_toFloat(readobj[0])
-      print(str(daread+" | "+str(val)))
+        if (data[i][1]=="ST"):
+          val=iec61850.MmsValue_getBitStringAsIntegerBigEndian(readobj[0])
+        else:
+          val=iec61850.MmsValue_toFloat(readobj[0])
+        print(str(daread+" | "+str(val)))
+    time.sleep(5)
     iec61850.IedConnection_close(con)
   iec61850.IedConnection_destroy(con)
     
